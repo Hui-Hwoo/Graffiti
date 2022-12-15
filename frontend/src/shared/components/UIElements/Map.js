74d3de198cf159c7d57c32b399859223a4964ddd
jerryasala <97819243+jerryasala@users.noreply.github.com>
2022-12-15 03:12:42 -0800

    Add comment to Map.js
    Pointed out the use of Maps in the project as an excellent idea

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -6,6 +6,8 @@ const Map = (props) => {
     const mapRef = useRef();
     const {center, zoom} = props;
 
+    /* Hui, I think it's really impressive that you used maps in this project.
+    It is such an excellent feature to include. Well done!*/
     useEffect(() => {
         const map = new window.google.maps.Map(mapRef.current, {
             center: center,
