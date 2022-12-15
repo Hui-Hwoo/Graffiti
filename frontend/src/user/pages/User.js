ed033a8bafaab81f84bd0b5d722e9d050a8349ab
jerryasala <97819243+jerryasala@users.noreply.github.com>
2022-12-15 02:51:19 -0800

    Add comment to User.js
    Added positive feedback on project folder structure

--- frontend/src/user/pages/User.js
+++ frontend/src/user/pages/User.js
@@ -4,6 +4,13 @@ import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import { useHttpClient } from "../../shared/hooks/hook-http";
 import UserList from "../components/UserList";
 
+/* Hui, I think you did a great job with your folder structure and how components
+are organized into well-named folders. For example, a component like "ErrorModal"
+that pertains to the UI of your web app intuitively goes into a subfolder in
+the components folder named "UIElements", which makes it easy to find component files
+within the project folder. Great job!*/
+
+
 const Users = () => {
     const [loadedUser, setLoadedUser] = useState();
     const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -33,5 +40,8 @@ const Users = () => {
         </React.Fragment>
     );
 };
+
+
+// Prop types here*
 
 export default Users;
